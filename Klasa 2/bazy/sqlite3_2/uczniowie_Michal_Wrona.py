f09e8301ce15f46a7170c7e0680863f58612bc78
kl3ag1 <kl3ag1@komp14.lo1cg.org>
2017-10-27 07:14:09 +0200

    Update

--- Klasa 2/bazy/sqlite3_2/uczniowie_Michal_Wrona.py
+++ Klasa 2/bazy/sqlite3_2/uczniowie_Michal_Wrona.py
@@ -15,7 +15,6 @@ def kw_a(cur):
         WHERE tbUczniowie.KlasaID = tbKlasy.IDKlasy
         AND tbKlasy.Klasa = '1A'
     """)
-    
     wyniki(cur)
         
 def kw_b(cur):
@@ -23,7 +22,6 @@ def kw_b(cur):
         SELECT MAX(EgzHum)
         FROM tbUczniowie
     """)
-    
     wyniki(cur)
     
 def kw_c(cur):
@@ -33,7 +31,6 @@ def kw_c(cur):
         WHERE tbUczniowie.KlasaID = tbKlasy.IDKlasy
         AND tbKlasy.Klasa = '1A'
     """)
-
     wyniki(cur)
     
 def kw_d(cur):
@@ -43,21 +40,48 @@ def kw_d(cur):
         WHERE tbOceny.UczenID = tbUczniowie.IDUcznia
         AND tbUczniowie.Nazwisko = 'Nowak'
     """)
-    
     wyniki(cur)
     
 def kw_e(cur):
     cur.execute("""
-        SELECT AVG(tbOceny.Ocena)
-        FROM tbOceny, tbPrzedmioty
-        WHERE tbOceny.PrzedmiotID = tbPrzedmioty.IDPrzedmiotu
-        AND tbPrzedmioty.Przedmiot = 'fizyka'
-        AND tbOceny.Datad > '2012-10-01'
-        AND tbOceny.Datad < '2012-10-31'
+        SELECT AVG(Ocena)
+        FROM tbOceny
+        WHERE PrzedmiotID = 6
+        AND strftime('%m', Datad) LIKE '10'
+        """)
+        ##AND Datad > '2012-10-01'
+        ##AND Datad < '2012-10-31'
+    wyniki(cur)
+
+def kw_f(cur):
+    cur.execute("""
+        UPDATE tbUczniowie
+        SET EgzJez = ?
+        WHERE Imie = 'Paulina'
+        AND Nazwisko = 'Dziedzic'
+    """, [35])
+    
+def kw_g(cur):
+    cur.execute("""
         
     """)
     
-    wyniki(cur)
+def dodaj(cur):
+    cur.execute("""
+        INSERT INTO tbKlasy
+        VALUES (?, ?, ?, ?)
+    """, [None, '3C', 2015, 2017])
+
+def aktualizuj(cur):
+    cur.execute("""
+        UPDATE tbKlasy
+        SET klasa = ?
+        WHERE idKlasy = ?
+    """, ['3D', 13])
+    
+
+def usun(cur):
+    cur.execute('DELETE FROM tbKlasy WHERE klasa = ? AND roknaboru = ?', ['3B', 2015])
 
 def main(args):
     
@@ -65,7 +89,13 @@ def main(args):
     cur = con.cursor()
     con.row_factory = sqlite3.Row
     
-    kw_e(cur)
+    #dodaj(cur)
+    #aktualizuj(cur)
+    #usun(cur)
+    kw_f(cur)
+    con.commit()
+    #wyniki(cur.execute('SELECT * FROM tbKlasy'))
+    wyniki(cur.execute("""SELECT * FROM tbUczniowie WHERE Imie = 'Paulina' AND Nazwisko = 'Dziedzic'"""))
     
     return 0
 
