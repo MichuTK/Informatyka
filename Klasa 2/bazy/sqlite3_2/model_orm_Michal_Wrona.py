beab404f40681bb773d9645bf31eb6fae1f0a31b
kl3ag1 <kl3ag1@komp14.lo1cg.org>
2017-11-14 11:41:02 +0100

    Poprawienie modelu bazy

--- Klasa 2/bazy/sqlite3_2/model_orm_Michal_Wrona.py
+++ Klasa 2/bazy/sqlite3_2/model_orm_Michal_Wrona.py
@@ -2,7 +2,7 @@
 
 from peewee import *
 
-baza_plik = "pracownicy.sqlite3"
+baza_plik = "szkola.db"
 
 baza = SqliteDatabase(baza_plik)  # ':memory:'
 
@@ -16,28 +16,36 @@ class Klasa(BazaModel):
     nazwa = CharField(null=False)
     rok_naboru = IntegerField(null=False)
     rok_matury = IntegerField(null=False)
-    
+
 class Przedmiot(BazaModel):
     nazwa = CharField(null=False)
     imien = CharField(null=False)
     nazwiskon = CharField(null=False)
     plecn = IntegerField(null=False)
 
-class Ocena(BazaModel):
-    datad = DateField()
-    uczen_id = ForeignKeyField(Uczen, related_name='id')
-    przedmiot_id = ForeignKeyField(Przedmiot, related_name='id')
-    ocena = DecimalField(null=False)
-
 class Uczen(BazaModel):
     imie = CharField(null=False)
     nazwisko = CharField(null=False)
     plec = IntegerField(null=False)
-    klasa_id = ForeignKeyField(Klasa, related_name='id')
+    klasa_id = ForeignKeyField(Klasa, related_name='Uczen')
     egzhum = DecimalField(decimal_places=2, default=0, null=False)
     egzmat = DecimalField(decimal_places=2, default=0, null=False)
     egzjez = DecimalField(decimal_places=2, default=0, null=False)
 
-
+class Ocena(BazaModel):
+    datad = DateField()
+    uczen_id = ForeignKeyField(Uczen, related_name='Ocena')
+    przedmiot_id = ForeignKeyField(Przedmiot, related_name='O')
+    ocena = DecimalField(null=False)
 
 baza.connect()  # nawiązujemy połączenie z bazą
+
+def kwerenda_a():
+    query = (Uczen
+             .select(Uczen.imie, Uczen.nazwisko)
+             )
+
+    for obj in query:
+        print(obj.imie, obj.nazwisko)
+
+kwerenda_a()
