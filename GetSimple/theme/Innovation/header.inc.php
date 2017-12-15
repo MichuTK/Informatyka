b00d51f4e88e0735bd12d73d01ac9b55555f9a2c
Michal <mwrona141@gmail.com>
2017-12-15 07:55:38 +0100

    Dodanie obrazow

--- GetSimple/theme/Innovation/header.inc.php
+++ GetSimple/theme/Innovation/header.inc.php
@@ -16,49 +16,49 @@
 <meta charset="utf-8">
 
   <title><?php get_page_clean_title(); ?> - <?php get_site_name(); ?></title>
-	
+
 	<meta name="robots" content="index, follow">
 
 	<link href='//fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css'>
 	<link href="<?php get_theme_url(); ?>/assets/css/reset.css" rel="stylesheet">
 	<link href="<?php get_theme_url(); ?>/style.css?v=<?php echo get_site_version(); ?>" rel="stylesheet">
-	
+
 	<!--[if lt IE 9]>
 		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]--> 
-	
+	<![endif]-->
+
 	<!--[if lt IE 7 ]>
     <script src="<?php get_theme_url(); ?>/assets/js/dd_belatedpng.js"></script>
     <script> DD_belatedPNG.fix('img, .png_bg'); //fix any <img> or .png_bg background-images </script>
   <![endif]-->
 
 	<?php get_header(); ?>
-	
-</head> 
+
+</head>
 <body id="<?php get_page_slug(); ?>" >
-	
+
 	<!-- site header -->
 	<header>
 		<div class="header">
 			<div class="wrapper">
-				
+
 				<!-- logo/sitename -->
 				<a href="<?php get_site_url(); ?>" id="logo" ><?php get_site_name(); ?></a>
-				
+
 				<!-- main navigation -->
 				<nav id="main-nav">
 					<ul>
-						<?php get_navigation(get_page_slug(FALSE)); ?>
+						<?php get_i18n_navigation(return_page_slug()); ?>
 					</ul>
 				</nav>
 			</div>
 		</div>
-		
+
 		<!-- breadcrumbs: only show when NOT on homepage -->
 		<p class="breadcrumbs" >
 			<span class="wrapper">
 				<a href="<?php get_site_url(); ?>">Home</a> &nbsp;&nbsp;&#149;&nbsp;&nbsp; <?php Innovation_Parent_Link(get_parent(FALSE)); ?> <b><?php get_page_clean_title(); ?></b>
 			</span>
 		</p>
-		
+
   </header>
