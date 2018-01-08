783faa7ac38462c20f878f19d5b989c87902864c
MichuTK <tkmichu@gmail.com>
2018-01-08 18:28:25 +0100

    Dodanie funkcji odczytującej dane z pliku tekstowego

--- odczytcsv.py
+++ odczytcsv.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 #
 #  odczytcsv.py
-#
+#  
 
-import  csv
+import csv
 
 def main(args):
+    plik = open("samochody.txt")
+    tekst = plik.read()
+    plik.close()
+    print(tekst)
     return 0
 
 if __name__ == '__main__':
