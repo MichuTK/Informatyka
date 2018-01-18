7c1c10e3df80a286b1a531160950c4a82e799677
Tomek2854 <twiniarski8@gmail.com>
2018-01-18 13:30:47 +0100

    Programowanie obiektowe

---
+++ Cpp/ul_ulamek.h
@@ -0,0 +1,17 @@
+/*
+ * Plik nagłówkowy klasy Ulamek 
+ * 
+ */
+
+class Ulamek {
+private:
+    int licznik; // Deklaracja składowej właściwości
+    int mianownik;
+public:
+    Ulamek(int, int); // Deklaracja konstruktora
+    void zapisz(int, int); // Deklaracja metody
+    void wypisz();
+    int get_l();
+    int get_m();
+    void skracaj(); // Metoda drukuje skróconą postać ułamka
+};
