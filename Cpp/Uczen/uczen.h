1ec652b557352d2f5bb59a5f63c75a5aea0ec887
Michu <tkmichu@gmail.com>
2018-01-29 20:29:30 +0100

    Prada domowa klasy

---
+++ Cpp/Uczen/uczen.h
@@ -0,0 +1,14 @@
+#ifndef UCZEN_H
+#define UCZEN_H
+class Uczen{
+private:
+	string imie;
+	string nazwisko;
+	string klasa;
+	int ocena;
+public:
+	Uczen(string, string);
+	void srednia(int);
+	void ustaw_klase("3A");
+};
+#endif
