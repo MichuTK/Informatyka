c2edd44eaab91a45e44d44005a45ee74cd927642
Michal <mwrona141@gmail.com>
2018-04-05 12:56:44 +0200

    Liczba Pi - Wizualizacja

--- p5js/p5/empty-example/sketch.js
+++ p5js/p5/empty-example/sketch.js
@@ -1,38 +1,54 @@
-//     ileKw = int(input("Podaj ilość losowanych punktów: "))
-//     ileKo = 0
-//     r = 1
+szerokosc = 640;
+wysokosc = 480;
 
-//     for i in range(ileKw):
-//         x = random.random()
-//         y = random.random()
-//         if x**2 + y**2 <= r**2:
-//             ileKo += 1
-
-//     pi = 4 * ileKo / ileKw
-//     print("Wartość PI: ", pi)
 ileKw = prompt("Podaj ilość punktów: ", 100);
-ileKo = 0;
+ileKw = parseInt(ileKw)
 r = prompt("Podaj promień koła: ", 100);
+ileKo = 0;
 
 function getRnd(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
+lx = [];
+ly = [];
+
 for (i = 0; i < ileKw; i++){
 	x = Math.floor(getRnd(-r, r));
-	y = getRnd(-r, r);
+	y = Math.floor(getRnd(-r, r));
 	// console.log(x);
+	// console.log(y);
+	lx[i] = x;
+	ly[i] = y;
 	if (x*x + y*y <= r*r){
 		ileKo++;
 	}
 }
 
-console.log("Liczba Pi: " + (4 * ileKo / ileKw))
+console.log(lx);
+pi = 4 * ileKo / ileKw;
+alert("Liczba Pi: " + pi);
+
 
 function setup() {
-  // put setup code here
+  createCanvas(szerokosc, wysokosc);
+	background('#fff');
+	stroke('#000');
+	ellipse(szerokosc / 2, wysokosc / 2, 2 * r, 2 * r);
+	line(szerokosc / 2, 0, szerokosc / 2, wysokosc); // oś y
+	line(0, wysokosc / 2, szerokosc, wysokosc / 2); // oś x
 }
 
+x_offset = szerokosc / 2;
+y_offset = wysokosc / 2;
+
 function draw() {
-  // put drawing code here
+  for (i = 0; i < ileKw; i++){
+		if(lx[i]*lx[i] + ly[i]*ly[i] <= r*r){
+			stroke('#f00');
+		} else {
+			stroke('#000');
+		}
+		point(lx[i] + x_offset, ly[i] + y_offset);
+	}
 }
